1c58200d70a12e83dc27ba8c44232e01511597d9
anandan1998 <anandan1998@gmail.com>
2024-05-10 23:36:33 +0200

    Added the login page

--- android/app/src/main/kotlin/com/example/flutter_map_me/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_map_me
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ananthapathmanabhanr/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ananthapathmanabhanr/Downloads/global-flutter/flutter_project_outbreak/map-me/flutter_map_me"
+export "FLUTTER_APPLICATION_PATH=/Users/ananthapathmanabhanr/Downloads/global-flutter/hetvi/flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
